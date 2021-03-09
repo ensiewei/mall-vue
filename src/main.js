b677d48f8ce5331c6f77fc3f85110c4e1c0a49f3
ensiewei <ensiewei@gmail.com>
2021-03-09 17:17:38 +0800

    add axios and cookies

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import installElementPlus from './plugins/element'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueCookie from 'vue3-cookies'
 
-const app = createApp(App).use(router)
+const app = createApp(App).use(router).use(VueAxios, axios).use(VueCookie)
+
 installElementPlus(app)
 app.mount('#app')
